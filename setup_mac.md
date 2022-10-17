78ae14ad9b47906f50c5a4748c08b9573461d8cd
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2022-10-17 23:29:21 +0200

    Update mac setup commands

---
+++ setup_mac.md
@@ -0,0 +1,24 @@
+# macOS configuration
+
+Auto configuration: `sh setup_mac.sh`
+
+## Finder
+
+### Shortcuts
+
+- Copy directory/file path `CMD + Option + C`
+- Quick search  `CMD + Shift + G`
+
+### Options (`CMD + J`)
+
+- Text size `12`
+- Icon size `54x54`
+- Grid spacing `Fourth line`
+- Show item info
+- Show icon preview
+
+### View
+
+- Show Path Bar
+- Show Status Bar
+
--- setup_mac.sh
+++ setup_mac.sh
@@ -1,4 +1,28 @@
+# --- apps ---
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install important packages
+brew install \\
+  wget \\  - curl alternative
+  exa  \\  - ls alternative
+  git  \\  - Git
+  nvm  \\  - Node.js Package Manager
+  gh   \\  - GitHub CLI
+
+# Adjust terminal
+brew install zsh
+wget <https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh>
+sh install.s
+
+# --- macOS config ---
+
 # remove shadow from screenshot (default false)
 defaults write com.apple.screencapture disable-shadow -bool true; killall SystemUIServer
 # save screeshots as jpeg (default png)
 defaults write com.apple.screencapture type jpg; killall SystemUIServer
+# display hidden files
+defaults write com.apple.finder AppleShowAllFiles YES
+
+killall Finder
